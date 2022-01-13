da6abb6f543d0302b93b54b6fd71e8ba6cb1ea99
Tzappiello <tzappiello@gmail.com>
2022-01-13 15:23:37 -0300

    Logica do Campo

--- src/br/com/zappi/projeto/modelo/Campo.java
+++ src/br/com/zappi/projeto/modelo/Campo.java
@@ -1,5 +1,21 @@
 package br.com.zappi.projeto.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 
+	private final int linha;
+	private final int coluna;
+	
+	private boolean aberto;
+	private boolean minado;
+	private boolean marcado;
+
+	private List<Campo> vizinhos = new ArrayList();
+
+	Campo(int linha, int coluna){
+		this.linha = linha;
+		this.coluna = coluna;
+	}
 }
