21d136e7678375967cbfbec209c178b060984abc
Tzappiello <tzappiello@gmail.com>
2022-01-13 14:53:48 -0300

    Versao 0.1

---
+++ teste/br/com/zappi/projeto/Teste.java
@@ -0,0 +1,14 @@
+package br.com.zappi.projeto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
