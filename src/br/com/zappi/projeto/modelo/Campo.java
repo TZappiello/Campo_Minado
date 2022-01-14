6efa2da51775386f9fcd9ea35c754d1f18843ad2
Tzappiello <tzappiello@gmail.com>
2022-01-14 11:22:00 -0300

    Versao 0.4

--- src/br/com/zappi/projeto/modelo/Campo.java
+++ src/br/com/zappi/projeto/modelo/Campo.java
@@ -79,13 +79,52 @@ public class Campo {
 	public boolean isMarcado() {
 		return marcado;
 	}
-	
+
 	public boolean isAberto() {
 		return aberto;
 	}
 
 	public boolean isFechado() {
 		return !isAberto();
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	boolean objetivoAlcancado() {
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		return desvendado || protegido;
+	}
+
+	long minasNaVizinhaca() {
+		return vizinhos.stream().filter(v -> v.minado).count();
+
+	}
+
+	void reiniciar() {
+		aberto = false;
+		minado = false;
+		marcado = false;
+	}
+
+	public String toString() {
+		if (marcado) {
+			return "x";
+		} else if (aberto && minado) {
+			return "*";
+		} else if (aberto && minasNaVizinhaca() > 0) {
+			return Long.toString(minasNaVizinhaca());
+		} else if (aberto) {
+			return " ";
+		} else {
+			return "?";
+		}
 	}
 
 }
