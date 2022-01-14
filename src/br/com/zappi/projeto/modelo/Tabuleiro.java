b94ba01e358cbca06e9a54be847ff9524d6d5287
Tzappiello <tzappiello@gmail.com>
2022-01-14 11:41:46 -0300

    Versao 0.2 Tabuleiro

--- src/br/com/zappi/projeto/modelo/Tabuleiro.java
+++ src/br/com/zappi/projeto/modelo/Tabuleiro.java
@@ -1,5 +1,45 @@
 package br.com.zappi.projeto.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampo();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+	private void gerarCampo() {
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}
+		}
+	}
+	
+	private void associarVizinhos() {
+		for(Campo c1: campos) {
+			for(Campo c2: campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+
+	private void sortearMinas() {
+		
+	}
+
 
 }
